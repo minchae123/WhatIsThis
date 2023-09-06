b447eec24eb76275f9dcaff347c8fa7cfd2c4664
dksgudwn <dksgudwn620@gmail.com>
2023-09-06 18:30:21 +0900

    게 드래그

--- Assets/01_Scripts/dksgudwn/Crab.cs
+++ Assets/01_Scripts/dksgudwn/Crab.cs
@@ -14,16 +14,20 @@ public class Crab : MonoBehaviour
     {
 
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        //int collisionSize = collision.gameObject.GetComponent<Crab>().crabData.Number;
-        //if (collision.tag == "Crab" && collisionSize == this.crabData.Number)
-        //{
-        //    Debug.Log("사이즈 같은 게");
-        //}
-        //else if (collision.tag == "Crab" && collisionSize != this.crabData.Number)
-        //{
-        //    Debug.Log("사이즈 다른 게");
-        //}
+        if (ColliderCheck)
+        {
+            int collisionSize = collision.gameObject.GetComponent<Crab>().crabData.Number;
+            if (collision.tag == "Crab" && collisionSize == this.crabData.Number)
+            {
+                Debug.Log("합침");
+            }
+            else if (collision.tag == "Crab" && collisionSize != this.crabData.Number)
+            {
+                Debug.Log("사이즈 다른 게");
+            }
+        }
     }
 }
