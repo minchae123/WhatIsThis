996281542121034fdffe73222b34d4d06ff5d0ac
Woo Hyeok <pwh060720@gmail.com>
2023-09-07 20:26:45 +0900

    클릭 dotween 하는중

--- Assets/01_Scripts/PWH/ManyTouch/ScreenTouch.cs
+++ Assets/01_Scripts/PWH/ManyTouch/ScreenTouch.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
 
 public class ScreenTouch : MonoBehaviour
 {
     int count = 0;
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] Ease ease;
     // Start is called before the first frame update
     void Start()
     {
-
+        text.transform.DOScale(1.05f, 0.5f).SetLoops(-1, LoopType.Yoyo);
     }
 
     // Update is called once per frame
     public void BtnClick()
     {
+        Sequence seq = DOTween.Sequence();
+
         count++;
         Debug.Log(count);
+        Debug.Log(count);
+        text.transform.DOKill(false);
+        text.transform.DOKill(true);
+        text.transform.DOScale(1.05f, 0.1f).SetLoops(-1, LoopType.Yoyo);
     }
 }
