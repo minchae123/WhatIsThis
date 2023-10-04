58e7ee8b8b948f315ade1259f0bc6ebf66fef05e
Woo Hyeok <pwh060720@gmail.com>
2023-10-04 19:13:41 +0900

    수정

--- Assets/01_Scripts/PWH/ManyTouch/ScreenTouch.cs
+++ Assets/01_Scripts/PWH/ManyTouch/ScreenTouch.cs
@@ -6,26 +6,77 @@ using TMPro;
 
 public class ScreenTouch : MonoBehaviour
 {
-    int count = 0;
-    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] protected TextMeshProUGUI Score;
+
     [SerializeField] Ease ease;
     private Sequence seq;
-    // Start is called before the first frame update
+
+    protected string Scoretext;
+    [SerializeField] private float timeLimit = 20;
+
+    [SerializeField] protected int totalScore;
+    private int clickCount;
+
+    private int minCount = 3;
+    private int maxCount = 7;
+
+    private bool isClick = false;
     void Start()
     {
         seq = DOTween.Sequence();
         seq.Append(text.transform.DOScale(1.05f, 0.5f).SetLoops(-1, LoopType.Yoyo));
+
+        StartCoroutine(Timer());
     }
 
     // Update is called once per frame
     public void BtnClick()
     {
-        seq.Kill();
-        
-        Sequence s = DOTween.Sequence();
-        s.Kill();
+        StartCoroutine(ClickDotween());
 
-        s.Append(text.transform.DOScale(1.2f, 0.1f).SetLoops(2, LoopType.Yoyo));
+        clickCount = Random.Range(minCount, maxCount);
+        totalScore += clickCount;
+
+        text.text = totalScore.ToString();
+
+        isClick = true;
+    }
+
+    private IEnumerator ClickDotween()
+    {
+        seq.Append(text.transform.DOScale(1.05f, 0.5f).SetLoops(-1, LoopType.Yoyo));
+        yield return new WaitForSeconds(.1f);
+        seq.Append(text.transform.DOScale(1.5f, 0.1f).SetLoops(2, LoopType.Yoyo));
+    }
+
+    private void Update()
+    {
+        Score.text = timeLimit.ToString();
+        Debug.Log(Score.text);
+    }
+
+    IEnumerator Timer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+
+            if (isClick)
+            {
+                timeLimit--;
+                seq.Kill();
+
+                Sequence s = DOTween.Sequence();
+                s.Kill();
+
+                if (timeLimit == 0)
+                {
+                    //미니게임이 끝났을 때 해야할 것
+                }
+            }
+        }
+        //s.Append(text.transform.DOScale(1.2f, 0.1f).SetLoops(2, LoopType.Yoyo));
         //count++;
     }
 }
