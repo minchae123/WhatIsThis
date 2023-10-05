354f68d067b651089fb6bacb5348aa8dc7ee483e
dksgudwn <dksgudwn620@gmail.com>
2023-10-05 18:20:45 +0900

    여러 개의 게가 중복으로 합쳐지는 버그 수정

--- Assets/01_Scripts/dksgudwn/Crab.cs
+++ Assets/01_Scripts/dksgudwn/Crab.cs
@@ -16,14 +16,7 @@ public class Crab : MonoBehaviour
         _crabPrefab = GetComponent<Crab>();
         _spriteRenderer.sprite = crabData.Image;
     }
-    private void Start()
-    {
 
-    }
-    private void Update()
-    {
-
-    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -32,16 +25,16 @@ public class Crab : MonoBehaviour
             int collisionSize = collision.gameObject.GetComponent<Crab>().crabData.Number;
             if (collision.tag == "Crab" && collisionSize == this.crabData.Number)
             {
-                //
                 CrabManager.Instance.MergeCrab(collisionSize + 1, transform.position);
                 StartCoroutine(delayCoroutine(10f, collision));
                 Destroy(gameObject);
                 Destroy(collision.gameObject);
             }
-            else if (collision.tag == "Crab" && collisionSize != this.crabData.Number)
-            {
-                Debug.Log("사이즈 다른 게");
-            }
+            //else if (collision.tag == "Crab" && collisionSize != this.crabData.Number)
+            //{
+            //    Debug.Log("사이즈 다른 게");
+            //}
+            ColliderCheck = false;
         }
     }
 
