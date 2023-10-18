140aa6106013e287f363c5bcb7899d39e4510555
Inheoseongbin <seongbin529@gmail.com>
2023-10-18 20:27:29 +0900

    메인합치기

--- Assets/01_Scripts/Obstacle/Obstacle.cs
+++ Assets/01_Scripts/Obstacle/Obstacle.cs
@@ -4,49 +4,48 @@ using UnityEngine;
 
 public enum Obstacles
 {
-	WHALE,
-	SHARK,
-	BOBBER
+    WHALE,
+    SHARK,
+    BOBBER
 }
 
 public class Obstacle : MonoBehaviour
 {
-	public Obstacles type;
+    public Obstacles type;
 
-	private WhaleShark whaleshark;
-	private Bobber bobber;
+    private WhaleShark whaleshark;
+    private Bobber bobber;
 
-	private void Awake()
-	{
-		switch (type)
-		{
-			case Obstacles.WHALE:
-			case Obstacles.SHARK:
-				whaleshark = GetComponentInChildren<WhaleShark>();
-				break;
-			case Obstacles.BOBBER:
-				bobber = GetComponent<Bobber>();
-				break;
-		}
-	}
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-		if(collision.CompareTag("Crab"))
-		{
-			switch (type)
-			{
-				case Obstacles.WHALE:
-				case Obstacles.SHARK:
-					whaleshark.Yam();
-					Destroy(collision.gameObject);
-					break;
-				case Obstacles.BOBBER:
-					bobber.ResetPos();
-					collision.gameObject.transform.parent = transform;
-					Destroy(collision.GetComponent<Dragger>());
-					Destroy(collision.GetComponent<Crab>());
-					break;
-			}
-		}
-	}
+    private void Awake()
+    {
+        switch (type)
+        {
+            case Obstacles.WHALE:
+            case Obstacles.SHARK:
+                whaleshark = GetComponentInChildren<WhaleShark>();
+                break;
+            case Obstacles.BOBBER:
+                bobber = GetComponent<Bobber>();
+                break;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Crab"))
+        {
+            switch (type)
+            {
+                case Obstacles.WHALE:
+                case Obstacles.SHARK:
+                    whaleshark.Yam();
+                    Destroy(collision.gameObject);
+                    break;
+                case Obstacles.BOBBER:
+                    bobber.ResetPos();
+                    collision.gameObject.transform.parent = transform;
+                    collision.GetComponent<Dragger>().enabled = false;
+                    break;
+            }
+        }
+    }
 }
