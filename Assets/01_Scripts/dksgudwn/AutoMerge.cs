b41a13be7e580e80b85b5092784954be8c599c5d
dksgudwn <dksgudwn620@gmail.com>
2023-11-01 20:27:23 +0900

    여기까쥐ㅣㅣㅣ

---
+++ Assets/01_Scripts/dksgudwn/AutoMerge.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMerge : MonoBehaviour
+{
+    List<PoolableMono> EnableCrab;
+
+    public void Auto()
+    {
+        for (int i = 0; i < GameManager.Instance.poolingListSO.list.Count; i++)
+        {
+
+        }
+    }
+}
