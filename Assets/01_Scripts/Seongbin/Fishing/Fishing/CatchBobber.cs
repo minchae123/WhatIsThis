cd12aa549dd0c5995c31a959e2db302f41d05cf1
Inheoseongbin <seongbin529@gmail.com>
2023-11-02 00:20:19 +0900

    낚시던지는거랑비주얼업그레이드를하였지만마음에들지않아나중에많이많이많이많이바꿀거다

--- Assets/01_Scripts/Seongbin/Fishing/Fishing/CatchBobber.cs
+++ Assets/01_Scripts/Seongbin/Fishing/Fishing/CatchBobber.cs
@@ -2,9 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using UnityEngine.Events;
+using System;
 
 public class CatchBobber : MonoBehaviour
 {
+    public static Action bobberThrow;
+    public static Action resetGravity;
+    public static Action crabBite;
+    public static Action bobberUp;
 
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+        bobberThrow += BobberThrow;
+        resetGravity += ResetGravity;
+        crabBite += BiteCrab;
+        bobberUp += BobberUp;
+    }
+
+    //³¬½ËÁÙ ´øÁö±â
+    public void BobberThrow()
+    {
+        _rb.gravityScale = 1;
+        _rb.AddForce(new Vector2(3, 0), ForceMode2D.Impulse);
+    }
+    //³¬½ÃÂî Áß·Â 0À¸·Î
+    public void ResetGravity() => _rb.gravityScale = 0;
+
+    //ÀÔÁú ¿ÔÀ»¶§ Èçµé±â
+    public void BiteCrab() => transform.DOShakePosition(3.5f);
+
+    //³¬¾ÒÀ»¶§ À§·Î µå´Â°Å
+    public void BobberUp()
+    {
+        transform.position = new Vector3(transform.position.x + .5f, transform.position.y + 3, transform.position.z);
+    }
 }
