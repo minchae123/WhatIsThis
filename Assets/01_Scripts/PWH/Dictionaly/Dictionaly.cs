a7dd74bf35922c988f4c03054c235a5a3a158e0a
Woo Hyeok <pwh060720@gmail.com>
2023-11-05 23:55:33 +0900

    이것저것 고친다

--- Assets/01_Scripts/PWH/Dictionaly/Dictionaly.cs
+++ Assets/01_Scripts/PWH/Dictionaly/Dictionaly.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class Dictionaly : MonoBehaviour
 {
@@ -9,6 +10,10 @@ public class Dictionaly : MonoBehaviour
     [SerializeField] private CrabSO[] _crabSO;
     [SerializeField] private GameObject BtnImage;
     [SerializeField] private Transform contentTr;
+    [SerializeField] private GameObject explain;
+    [SerializeField] private RectTransform rty;
+
+    private readonly string _imageName = "Visual";
 
     private void Awake()
     {
@@ -22,7 +27,7 @@ public class Dictionaly : MonoBehaviour
 
             for (int j = 0; j < obj.GetComponentsInChildren<Image>().Length; j++)
             {
-                if (obj.GetComponentsInChildren<Image>()[j].name == "Visual")
+                if (obj.GetComponentsInChildren<Image>()[j].name == _imageName)
                 {
                     Image visual = obj.GetComponentsInChildren<Image>()[j];
                     visual.sprite = _crabSO[i].Image;
@@ -33,6 +38,19 @@ public class Dictionaly : MonoBehaviour
 
     public void ShowExplain()
     {
+        Debug.Log("asfghjhgfwerthvc");
 
+        //explain.transform.position = transform.position;
+
+        //explain.transform.DOScale(new Vector2(800, 1400), 1);
+        //explain.transform.DOMove(Vector2.zero, 1);
+
+        var seq = DOTween.Sequence();
+
+        // DOScale 의 첫 번째 파라미터는 목표 Scale 값, 두 번째는 시간입니다.
+        seq.Append(explain.transform.DOScale(1.1f, 0.2f));
+        seq.Append(explain.transform.DOScale(1f, 0.1f));
+
+        seq.Play();
     }
 }
