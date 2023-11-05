178e574b8ae8604023804234fc356efe6b74196e
dksgudwn <dksgudwn620@gmail.com>
2023-11-06 00:53:43 +0900

    두 개 찾고 위치로 옮기기 했음 시퀀스를 이용하여 움직였을 때 합쳐지기 하기러기

--- Assets/01_Scripts/dksgudwn/AutoMerge.cs
+++ Assets/01_Scripts/dksgudwn/AutoMerge.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
@@ -5,23 +6,37 @@ using UnityEngine;
 
 public class AutoMerge : MonoBehaviour
 {
+    Sequence seq = DOTween.Sequence();
 
 
     public void Auto()
     {
-        //enableCrab.Clear();
-        //for (int i = 0; i < CrabSpawnManager.Instance.crabs.Count; i++)
-        //{
-        //    // 활성화되어 있으면 리스트에 추가
-        //    if (CrabSpawnManager.Instance.crabs[i].gameObject.activeSelf)
-        //    {
-        //        enableCrab.Add(CrabSpawnManager.Instance.crabs[i].gameObject);
-        //    }
-        //}
+        int randX = Random.Range(-2, 2);
+        int randY = Random.Range(-4, 4);
+        Vector2 randPos = new Vector2(randX, randY);
+
         for (int i = 0; i < CrabSpawnManager.Instance.crabs.Count; i++)
         {
-            FindNum(CrabSpawnManager.Instance.crabs[i].name);
+            for (int j = i + 1; j < CrabSpawnManager.Instance.crabs.Count; j++)
+            {
+                if (CrabSpawnManager.Instance.crabs[i].crabData.Number == CrabSpawnManager.Instance.crabs[j].crabData.Number)
+                {
+                    seq.Append(CrabSpawnManager.Instance.crabs[i].transform.DOMove(randPos, 1));
+                    seq.Join(CrabSpawnManager.Instance.crabs[j].transform.DOMove(randPos, 1));
+
+                    StartCoroutine(OnCheck(i));
+                    //CrabSpawnManager.Instance.crabs[i].GetComponent<Crab>().ColliderCheck = true;
+                    //CrabSpawnManager.Instance.crabs[j].GetComponent<Crab>().ColliderCheck = true;
+                }
+            }
         }
+    }
+
+    IEnumerator OnCheck(int index)
+    {
+        ///////////seq.Append(CrabSpawnManager.Instance.crabs[index].GetComponent<Crab>().ColliderCheck = true);
+        yield return new WaitForSeconds(0.05f);
+        CrabSpawnManager.Instance.crabs[index].GetComponent<Crab>().ColliderCheck = false;
     }
 
     int FindNum(string str)
